a68c1b4e242dfea014d0287387daca193b0fde7f
bomimee <158027966+bomimee@users.noreply.github.com>
2024-06-18 16:38:50 +0900

    login success

--- src/main/java/com/ict/interviewdotboot/config/SecurityConfig.java
+++ src/main/java/com/ict/interviewdotboot/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
         .csrf(csrf -> csrf.disable())
         .cors(cors -> cors.configurationSource(corsConfigurationSource()))
         .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/mypage/inquiry").permitAll()
+                        .requestMatchers("/api/login", "/api/logout", "/mypage/inquiry", "/api/create", "/api/users").permitAll()
                         .anyRequest().authenticated()
         )
         .logout(logout -> logout
