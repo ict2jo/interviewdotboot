df26d1ebc46194c01201f01aee7f89344e2f5577
yugh <yugh0922@gmail.com>
2024-06-27 01:24:11 +0900

    토스 하..

--- src/main/java/com/ict/interviewdotboot/mapper/TossMapper.java
+++ src/main/java/com/ict/interviewdotboot/mapper/TossMapper.java
@@ -1,5 +1,12 @@
 package com.ict.interviewdotboot.mapper;
 
-public class TossMapper {
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ict.interviewdotboot.vo.TossVO;
+
+@Mapper
+public interface TossMapper {
+
+    int savePayment(TossVO tossVO);
     
 }
