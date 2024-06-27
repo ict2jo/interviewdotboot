3141ddf948d4586f7aa6ea8b2824061b3be30139
bomimee <158027966+bomimee@users.noreply.github.com>
2024-06-27 17:06:36 +0900

    mail title fixed

--- src/main/java/com/ict/interviewdotboot/service/MailService.java
+++ src/main/java/com/ict/interviewdotboot/service/MailService.java
@@ -14,7 +14,7 @@ public class MailService {
 	public void sendEmail(String randomNumber, String toMail) {
 		try {
 			MailHandler sendMail = new MailHandler(mailSender);
-			sendMail.setSubject("[AIM 인증 메일입니다]");
+			sendMail.setSubject("[인터뷰닷 인증 메일입니다]");
 			
 			sendMail.setText("<div style='display: flex; flex-direction: column; align-items: center;'>"
 				+ "<h1>AIM 메일 인증</h1>"
