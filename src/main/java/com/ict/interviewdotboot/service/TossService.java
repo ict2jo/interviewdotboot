cd9403ed77341561c58da5986358037f0ab16965
yugh <yugh0922@gmail.com>
2024-06-30 05:40:39 +0900

    토스 결제취소 수정중

--- src/main/java/com/ict/interviewdotboot/service/TossService.java
+++ src/main/java/com/ict/interviewdotboot/service/TossService.java
@@ -89,10 +89,80 @@ public class TossService {
 
             return response.getStatusCode().is2xxSuccessful();
         } catch (Exception e) {
-            System.out.println("서비스 : "+e);
+            System.out.println("결제오류 : "+e);
         }
         return true;
     }
+    
+    public boolean cancelPayment(String paymentKey, String t_idx, String authorizationHeader, String id, String cancelReason) {
+        try {
+
+            System.out.println("페이먼츠키22"+paymentKey);
+            System.out.println("티티티아이디나오낭ㅇㅇㅇ" + t_idx);
+            System.out.println("아이디나오낭ㅇㅇㅇ" + id);
+            RestTemplate restTemplate = new RestTemplate();
+            // UTF-8 인코딩 설정 추가
+            restTemplate.getMessageConverters()
+                .add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", authorizationHeader);
+            headers.set("Content-Type", "application/json; charset=UTF-8");
+            
+            String requestJson = String.format(
+                "{\"paymentKey\":\"%s\",\"t_idx\":\"%s\",\"cancelReason\":\"%s\",\"userId\":\"%s\"}",
+                paymentKey, t_idx, cancelReason, id
+            );
+            
+            HttpEntity<String> entity = new HttpEntity<>(requestJson, headers);
+            System.out.println("엔티티 "+entity);
+
+            ResponseEntity<String> response = restTemplate.exchange(
+                "https://api.tosspayments.com/v1/payments/"+paymentKey+"/cancel",
+                HttpMethod.POST,
+                entity,
+                String.class
+            );
+            System.out.println("############Response: " + response); // UTF-8로 변환된 응답 출력
+
+            // JSON 응답 파싱
+            ObjectMapper objectMapper = new ObjectMapper();
+            TossVO tvo = objectMapper.readValue(response.getBody(), TossVO.class);
+            System.out.println("취추치ㅜ치ㅜ치ㅜ칯파싱된 응답: " + tvo);
+
+            
+        if (tvo.getCancels() != null && !tvo.getCancels().isEmpty()) {
+            TossVO.Cancel cancel = tvo.getCancels().get(0);
+            String canceledAt = cancel.getCanceledAt();
+            int cancelAmount = cancel.getCancelAmount();
+
+            LocalDateTime canceledAtDateTime = LocalDateTime.parse(canceledAt, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+            String formattedCanceledAt = canceledAtDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+            tvo.setCanceledAt(formattedCanceledAt);
+            tvo.setCancelAmount(cancelAmount);
+            tvo.setPayCount(cancelAmount / 1000);
+            tvo.setPayStatus("취소완료");
+            tvo.setCancelReason(cancelReason);
+
+            System.out.println("제발제ㅏㅂㄹ"+tvo.getCancelAmount());
+            System.out.println("제발제ㅏㅂㄹ"+tvo.getCanceledAt());
+            System.out.println("제발제ㅏㅂㄹ"+tvo.getPayCount());
+            System.out.println("제발제ㅏㅂㄹ"+tvo.getPayStatus());
+            System.out.println("제발제ㅏㅂㄹ"+tvo.getCancelReason());
+            int cancelPaymentResult = tossMapper.cancelPayment(tvo);
+            int userPayCountResult = tossMapper.userPayCount2(tvo);
+
+            System.out.println("cancelPaymentResult: " + cancelPaymentResult);
+            System.out.println("userPayCountResult: " + userPayCountResult);
+        }
+
+        } catch (Exception e) {
+            System.out.println("취소오류 : "+e);
+        }
+        return true;
+    }
+    
+
 
     public List<TossVO> userPay(String id) {
         return tossMapper.userPay(id);
--- src/main/java/com/ict/interviewdotboot/vo/TossVO.java
+++ src/main/java/com/ict/interviewdotboot/vo/TossVO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
+import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -14,19 +15,33 @@ public class TossVO {
     private String orderId;
     private String orderName;
     private int amount;
+    private int cancelAmount;
     private String approvedAt;
     private int payCount;
     private int statusCount;
     private String payStatus;
-    private String refund;
+    private String cancelReason;
     private String paymentKey;
+    private String canceledAt;
     private String authorizationHeader;
     private EasyPay easyPay;
+    private List<Cancel> cancels;
 
     @Data
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class EasyPay {
         @JsonProperty("amount")
         private int amount;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Cancel {
+        private String transactionKey;
+        private String cancelReason;
+        private int cancelAmount;
+        private String canceledAt;
+        private int easyPayDiscountAmount;
+        private String cancelStatus;
     }
 }
