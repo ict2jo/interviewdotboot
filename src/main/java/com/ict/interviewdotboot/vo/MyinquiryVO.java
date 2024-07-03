04c4a7c89a9c21bde8053d30fba22e7733fadc9d
pyeonjoy <ppee1220@naver.com>
2024-07-03 18:14:40 +0900

    풀캘린더 왜안되냐 병합안할거임

--- src/main/java/com/ict/interviewdotboot/vo/MyinquiryVO.java
+++ src/main/java/com/ict/interviewdotboot/vo/MyinquiryVO.java
@@ -4,5 +4,5 @@ import lombok.Data;
 
 @Data
 public class MyinquiryVO {
-    private String i_idx, i_subject, i_content, i_active, id;
+    private String i_idx, i_subject, i_content, i_active, id,i_date;
 }
