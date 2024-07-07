30f80b8797d936945b46be165a420a4aa9c2dcbc
yugh <yugh0922@gmail.com>
2024-07-07 20:19:27 +0900

    이력서 수정중

--- src/main/java/com/ict/interviewdotboot/mapper/OpenaiMapper.java
+++ src/main/java/com/ict/interviewdotboot/mapper/OpenaiMapper.java
@@ -1,5 +1,7 @@
 package com.ict.interviewdotboot.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.ict.interviewdotboot.vo.MyuserVO;
@@ -8,8 +10,12 @@ import com.ict.interviewdotboot.vo.UserVO;
 
 @Mapper
 public interface OpenaiMapper {
-    int introduceUpdate(UserVO uvo);
+    int introduceUpdate(ResumeVO resumeVO);
     int update1(MyuserVO myuserVO);
     int update2(MyuserVO myuserVO);
     int insert(ResumeVO resumeVO);
+
+    List<ResumeVO> re_select(String u_idx);
+    List<ResumeVO> re_select2(String resume_idx);
+    int re_update(ResumeVO resumeVO);
 }
